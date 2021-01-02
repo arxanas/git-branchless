3020395c96f519c2a70da521d4d20f591582d628
Waleed Khan <me@waleedkhan.name>
2021-01-01 18:06:38 -0600

    build(rust): add Rust interop

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
 import setuptools
+from setuptools_rust import RustExtension
+
 
 if __name__ == "__main__":
-    setuptools.setup()
+    setuptools.setup(
+        rust_extensions=[
+            RustExtension(
+                "branchless.branchlessrust", "Cargo.toml", quiet=False, debug=True
+            )
+        ]
+    )
