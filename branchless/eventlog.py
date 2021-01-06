7384b3f0e08802f69ad6df24398210e52240e5e9
Waleed Khan <me@waleedkhan.name>
2021-01-05 21:17:39 -0600

    eventlog(rust): port remainder of functions to Rust

--- branchless/eventlog.py
+++ branchless/eventlog.py
@@ -7,15 +7,15 @@ they're still working on.
 """
 from typing import TYPE_CHECKING
 
-from .rust import (
-    PyCommitEvent,
-    PyEventLogDb,
-    PyEventReplayer,
-    PyHideEvent,
-    PyRefUpdateEvent,
-    PyRewriteEvent,
-    PyUnhideEvent,
-)
+from .rust import PyCommitEvent as CommitEvent
+from .rust import PyEventLogDb as EventLogDb
+from .rust import PyEventReplayer as EventReplayer
+from .rust import PyHideEvent as HideEvent
+from .rust import PyRefUpdateEvent as RefUpdateEvent
+from .rust import PyRewriteEvent as RewriteEvent
+from .rust import PyUnhideEvent as UnhideEvent
+from .rust import is_gc_ref as is_gc_ref
+from .rust import should_ignore_ref_updates as should_ignore_ref_updates
 
 if TYPE_CHECKING:
     from .rust import Event as Event
@@ -25,36 +25,15 @@ else:
         pass
 
 
-NULL_OID = "0" * 40
-"""Denotes the lack of an OID.
-
-This could happen e.g. when creating or deleting a reference.
-"""
-
-
-def is_gc_ref(ref_name: str) -> bool:
-    """Determine whether a given reference is used to keep a commit alive.
-
-    Args:
-      ref_name: The name of the reference.
-
-    Returns:
-      Whether or not the given reference is used internally to keep the
-      commit alive, so that it's not collected by Git's garbage collection
-      mechanism.
-    """
-    return ref_name.startswith("refs/branchless/")
-
-
-def should_ignore_ref_updates(ref_name: str) -> bool:
-    return ref_name in ["ORIG_HEAD", "CHERRY_PICK_HEAD"] or is_gc_ref(ref_name)
-
-
-CommitEvent = PyCommitEvent
-HideEvent = PyHideEvent
-RefUpdateEvent = PyRefUpdateEvent
-RewriteEvent = PyRewriteEvent
-UnhideEvent = PyUnhideEvent
-
-EventLogDb = PyEventLogDb
-EventReplayer = PyEventReplayer
+# Silence flake8.
+_ = (
+    CommitEvent,
+    EventLogDb,
+    EventReplayer,
+    HideEvent,
+    RefUpdateEvent,
+    RewriteEvent,
+    UnhideEvent,
+    is_gc_ref,
+    should_ignore_ref_updates,
+)
