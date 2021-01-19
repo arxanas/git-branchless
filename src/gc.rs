5e8feb72135257a176ba0ad0756d65739d502403
Waleed Khan <me@waleedkhan.name>
2021-01-19 12:36:19 -0800

    gc(rust): port `mark_commit_reachable` to Rust

--- src/gc.rs
+++ src/gc.rs
@@ -17,7 +17,7 @@ use pyo3::prelude::*;
 use crate::eventlog::{is_gc_ref, EventLogDb, EventReplayer};
 use crate::graph::{make_graph, BranchOids, CommitGraph, HeadOid, MainBranchOid};
 use crate::mergebase::MergeBaseDb;
-use crate::python::{clone_conn, map_err_to_py_err, TextIO};
+use crate::python::{clone_conn, make_repo_from_py_repo, map_err_to_py_err, PyOid, TextIO};
 use crate::util::{
     get_branch_oid_to_names, get_db_conn, get_head_oid, get_main_branch_oid, get_repo,
 };
@@ -51,6 +51,31 @@ fn find_dangling_references<'repo>(
         }
     }
     Ok(result)
+}
+
+/// Mark a commit as reachable.
+///
+/// Once marked as reachable, the commit won't be collected by Git's garbage
+/// collection mechanism until first garbage-collected by branchless itself
+/// (using the `gc` function).
+///
+/// Args:
+/// * `repo`: The Git repository.
+/// * `commit_oid`: The commit OID to mark as reachable.
+pub fn mark_commit_reachable(repo: &git2::Repository, commit_oid: git2::Oid) -> anyhow::Result<()> {
+    let ref_name = format!("refs/branchless/{}", commit_oid.to_string());
+    anyhow::ensure!(
+        git2::Reference::is_valid_name(&ref_name),
+        format!("Invalid ref name to mark commit as reachable: {}", ref_name)
+    );
+    repo.reference(
+        &ref_name,
+        commit_oid,
+        true,
+        "branchless: marking commit as reachable",
+    )
+    .with_context(|| format!("Creating reference {}", ref_name))?;
+    Ok(())
 }
 
 /// Run branchless's garbage collection.
@@ -87,6 +112,17 @@ pub fn gc<Out: Write>(out: &mut Out) -> anyhow::Result<()> {
 }
 
 #[pyfunction]
+fn py_mark_commit_reachable(py: Python, repo: PyObject, commit_oid: PyOid) -> PyResult<()> {
+    let repo = make_repo_from_py_repo(py, &repo)?;
+    let PyOid(commit_oid) = commit_oid;
+    map_err_to_py_err(
+        mark_commit_reachable(&repo, commit_oid),
+        "Could not mark commit as reachable",
+    )?;
+    Ok(())
+}
+
+#[pyfunction]
 fn py_gc(py: Python, out: PyObject) -> PyResult<()> {
     let mut text_io = TextIO::new(py, out);
     map_err_to_py_err(gc(&mut text_io), "Failed to run GC")?;
@@ -94,6 +130,7 @@ fn py_gc(py: Python, out: PyObject) -> PyResult<()> {
 }
 
 pub fn register_python_symbols(module: &PyModule) -> PyResult<()> {
+    module.add_function(pyo3::wrap_pyfunction!(py_mark_commit_reachable, module)?)?;
     module.add_function(pyo3::wrap_pyfunction!(py_gc, module)?)?;
     Ok(())
 }
