arxanas/git-branchless

commit 07d892e5c38cc49ab5f95104e8a35ee9e89ba2d7

refactor(eventlog): create `EventCursor` type

tests/test_undo.rs
@@ -4,7 +4,7 @@ use std::convert::Infallible;
 
 use std::rc::Rc;
 
-use branchless::eventlog::{EventLogDb, EventReplayer};
+use branchless::eventlog::{EventCursor, EventLogDb, EventReplayer};
 use branchless::formatting::Glyphs;
 use branchless::mergebase::MergeBaseDb;
 use branchless::util::{get_db_conn, GitExecutable};
@@ -108,7 +108,7 @@ impl Backend for CursiveTestingBackend {
 fn run_select_past_event(
     repo: &git2::Repository,
     events: Vec<CursiveTestingEvent>,
-) -> anyhow::Result<Option<isize>> {
+) -> anyhow::Result<Option<EventCursor>> {
     let glyphs = Glyphs::text();
     let conn = get_db_conn(&repo)?;
     let merge_base_db = MergeBaseDb::new(&conn)?;
@@ -126,12 +126,11 @@ fn run_select_past_event(
     )
 }
 
-fn run_undo_events(git: &Git, event_id: isize) -> anyhow::Result<(String, String)> {
+fn run_undo_events(git: &Git, event_cursor: EventCursor) -> anyhow::Result<(String, String)> {
     let repo = git.get_repo()?;
     let conn = get_db_conn(&repo)?;
     let mut event_log_db: EventLogDb = EventLogDb::new(&conn)?;
-    let mut event_replayer = EventReplayer::from_event_log_db(&event_log_db)?;
-    event_replayer.set_cursor(event_id);
+    let event_replayer = EventReplayer::from_event_log_db(&event_log_db)?;
     let input = "y";
     let mut in_ = input.as_bytes();
     let mut out = Vec::new();
@@ -144,6 +143,7 @@ fn run_undo_events(git: &Git, event_id: isize) -> anyhow::Result<(String, String
         &GitExecutable(git.git_executable.clone()),
         &mut event_log_db,
         &event_replayer,
+        event_cursor,
     )?;
     assert_eq!(result, 0);
 
@@ -231,7 +231,7 @@ fn test_undo_navigate() -> anyhow::Result<()> {
         {
             let screenshot1 = Default::default();
             let screenshot2 = Default::default();
-            let event_id = run_select_past_event(
+            let event_cursor = run_select_past_event(
                 &git.get_repo()?,
                 vec![
                     CursiveTestingEvent::Event('p'.into()),
@@ -242,9 +242,11 @@ fn test_undo_navigate() -> anyhow::Result<()> {
                     CursiveTestingEvent::Event(Key::Enter.into()),
                 ],
             )?;
-            insta::assert_debug_snapshot!(event_id, @r###"
+            insta::assert_debug_snapshot!(event_cursor, @r###"
             Some(
-                5,
+                EventCursor {
+                    event_id: 5,
+                },
             )
             "###);
             insta::assert_snapshot!(screen_to_string(&screenshot1), @r###"
@@ -339,7 +341,7 @@ fn test_undo_hide() -> anyhow::Result<()> {
             "###);
         }
 
-        let event_id = run_select_past_event(
+        let event_cursor = run_select_past_event(
             &git.get_repo()?,
             vec![
                 CursiveTestingEvent::Event('p'.into()),
@@ -348,15 +350,17 @@ fn test_undo_hide() -> anyhow::Result<()> {
                 CursiveTestingEvent::Event('y'.into()),
             ],
         )?;
-        insta::assert_debug_snapshot!(event_id, @r###"
+        insta::assert_debug_snapshot!(event_cursor, @r###"
         Some(
-            9,
+            EventCursor {
+                event_id: 9,
+            },
         )
         "###);
-        let event_id = event_id.unwrap();
+        let event_cursor = event_cursor.unwrap();
 
         {
-            let (stdout, stderr) = run_undo_events(&git, event_id)?;
+            let (stdout, stderr) = run_undo_events(&git, event_cursor)?;
             insta::assert_snapshot!(stderr, @"");
             insta::assert_snapshot!(stdout, @r###"
             Will apply these actions:
@@ -394,7 +398,7 @@ fn test_undo_move_refs() -> anyhow::Result<()> {
         git.commit_file("test1", 1)?;
         git.commit_file("test2", 2)?;
 
-        let event_id = run_select_past_event(
+        let event_cursor = run_select_past_event(
             &git.get_repo()?,
             vec![
                 CursiveTestingEvent::Event('p'.into()),
@@ -404,15 +408,17 @@ fn test_undo_move_refs() -> anyhow::Result<()> {
                 CursiveTestingEvent::Event('y'.into()),
             ],
         )?;
-        insta::assert_debug_snapshot!(event_id, @r###"
+        insta::assert_debug_snapshot!(event_cursor, @r###"
         Some(
-            3,
+            EventCursor {
+                event_id: 3,
+            },
         )
         "###);
-        let event_id = event_id.unwrap();
+        let event_cursor = event_cursor.unwrap();
 
         {
-            let (stdout, _stderr) = run_undo_events(&git, event_id)?;
+            let (stdout, _stderr) = run_undo_events(&git, event_cursor)?;
             insta::assert_snapshot!(stdout, @r###"
             Will apply these actions:
             1. Hide commit 96d1c37a create test2.txt

src/graph.rs
@@ -7,7 +7,7 @@ use std::collections::{HashMap, HashSet, VecDeque};
 use fn_error_context::context;
 use log::warn;
 
-use crate::eventlog::{CommitVisibility, Event, EventReplayer};
+use crate::eventlog::{CommitVisibility, Event, EventCursor, EventReplayer};
 use crate::mergebase::MergeBaseDb;
 
 /// The OID of the repo's HEAD reference.
@@ -156,6 +156,7 @@ fn walk_from_commits<'repo>(
     commit_oids: &CommitOids,
 ) -> anyhow::Result<CommitGraph<'repo>> {
     let mut graph: CommitGraph = Default::default();
+    let cursor = event_replayer.make_default_cursor();
 
     for commit_oid in &commit_oids.0 {
         let commit = repo.find_commit(*commit_oid);
@@ -198,7 +199,8 @@ fn walk_from_commits<'repo>(
                 break;
             }
 
-            let visibility = event_replayer.get_cursor_commit_visibility(current_commit.id());
+            let visibility =
+                event_replayer.get_cursor_commit_visibility(cursor, current_commit.id());
             let is_visible = match visibility {
                 Some(CommitVisibility::Visible) | None => true,
                 Some(CommitVisibility::Hidden) => false,
@@ -210,7 +212,7 @@ fn walk_from_commits<'repo>(
             };
 
             let event = event_replayer
-                .get_cursor_commit_latest_event(current_commit.id())
+                .get_cursor_commit_latest_event(cursor, current_commit.id())
                 .cloned();
             graph.insert(
                 current_commit.id(),
@@ -347,13 +349,16 @@ pub fn make_graph<'repo>(
     repo: &'repo git2::Repository,
     merge_base_db: &MergeBaseDb,
     event_replayer: &EventReplayer,
+    event_cursor: EventCursor,
     head_oid: &HeadOid,
     main_branch_oid: &MainBranchOid,
     branch_oids: &BranchOids,
     remove_commits: bool,
 ) -> anyhow::Result<CommitGraph<'repo>> {
-    let mut commit_oids: HashSet<git2::Oid> =
-        event_replayer.get_active_oids().into_iter().collect();
+    let mut commit_oids: HashSet<git2::Oid> = event_replayer
+        .get_cursor_active_oids(event_cursor)
+        .into_iter()
+        .collect();
     commit_oids.extend(branch_oids.0.iter().cloned());
     if let HeadOid(Some(head_oid)) = head_oid {
         commit_oids.insert(*head_oid);

src/eventlog.rs
@@ -437,14 +437,26 @@ struct EventInfo {
     event_classification: EventClassification,
 }
 
+/// Events up to this cursor (exclusive) are available to the caller.
+///
+/// The "event cursor" is used to move the event replayer forward or
+/// backward in time, so as to show the state of the repository at that
+/// time.
+///
+/// The cursor is a position in between two events in the event log.
+/// Thus, all events before to the cursor are considered to be in effect,
+/// and all events after the cursor are considered to not have happened
+/// yet.
+#[derive(Clone, Copy, Debug)]
+pub struct EventCursor {
+    event_id: isize,
+}
+
 /// Processes events in order and determine the repo's visible commits.
 #[derive(Debug)]
 pub struct EventReplayer {
     /// Events are numbered starting from zero.
     id_counter: isize,
-
-    /// Events up to this number (exclusive) are available to the caller.
-    cursor_event_id: isize,
 
     /// The list of observed events.
     events: Vec<Event>,
@@ -457,7 +469,6 @@ impl EventReplayer {
     fn new() -> Self {
         EventReplayer {
             id_counter: 0,
-            cursor_event_id: 0,
             events: vec![],
             commit_history: HashMap::new(),
         }
@@ -501,7 +512,6 @@ impl EventReplayer {
 
         let id = self.id_counter;
         self.id_counter += 1;
-        self.cursor_event_id = self.id_counter;
         self.events.push(event.clone());
 
         match &event {
@@ -575,12 +585,12 @@ impl EventReplayer {
         };
     }
 
-    fn get_cursor_commit_history(&self, oid: git2::Oid) -> Vec<&EventInfo> {
+    fn get_cursor_commit_history(&self, cursor: EventCursor, oid: git2::Oid) -> Vec<&EventInfo> {
         match self.commit_history.get(&oid) {
             None => vec![],
             Some(history) => history
                 .iter()
-                .filter(|event_info| event_info.id < self.cursor_event_id)
+                .filter(|event_info| event_info.id < cursor.event_id)
                 .collect(),
         }
     }
@@ -593,8 +603,12 @@ impl EventReplayer {
     ///
     /// Returns: Whether the commit is visible or hidden. Returns `None` if no
     /// history has been recorded for that commit.
-    pub fn get_cursor_commit_visibility(&self, oid: git2::Oid) -> Option<CommitVisibility> {
-        let history = self.get_cursor_commit_history(oid);
+    pub fn get_cursor_commit_visibility(
+        &self,
+        cursor: EventCursor,
+        oid: git2::Oid,
+    ) -> Option<CommitVisibility> {
+        let history = self.get_cursor_commit_history(cursor, oid);
         let event_info = *history.last()?;
         match event_info.event_classification {
             EventClassification::Show => Some(CommitVisibility::Visible),
@@ -610,8 +624,12 @@ impl EventReplayer {
     ///
     /// Returns: The most recent event that affected that commit. If this commit
     /// was not observed by the replayer, returns `None`.
-    pub fn get_cursor_commit_latest_event(&self, oid: git2::Oid) -> Option<&Event> {
-        let history = self.get_cursor_commit_history(oid);
+    pub fn get_cursor_commit_latest_event(
+        &self,
+        cursor: EventCursor,
+        oid: git2::Oid,
+    ) -> Option<&Event> {
+        let history = self.get_cursor_commit_history(cursor, oid);
         let event_info = *history.last()?;
         Some(&event_info.event)
     }
@@ -620,11 +638,11 @@ impl EventReplayer {
     ///
     /// Returns: The set of OIDs referring to commits which are thought to be
     /// active due to user action.
-    pub fn get_active_oids(&self) -> HashSet<git2::Oid> {
+    pub fn get_cursor_active_oids(&self, cursor: EventCursor) -> HashSet<git2::Oid> {
         self.commit_history
             .iter()
             .filter_map(|(oid, history)| {
-                if history.iter().any(|event| event.id < self.cursor_event_id) {
+                if history.iter().any(|event| event.id < cursor.event_id) {
                     Some(*oid)
                 } else {
                     None
@@ -633,22 +651,16 @@ impl EventReplayer {
             .collect()
     }
 
-    /// Set the event cursor to point to immediately after the provided event.
+    /// Create an event cursor pointing to immediately after the last event.
+    pub fn make_default_cursor(&self) -> EventCursor {
+        self.make_cursor(self.events.len().try_into().unwrap())
+    }
+
+    /// Create an event cursor pointing to immediately after the provided event ID.
     ///
-    /// The "event cursor" is used to move the event replayer forward or
-    /// backward in time, so as to show the state of the repository at that
-    /// time.
-    ///
-    /// The cursor is a position in between two events in the event log.
-    /// Thus, all events before to the cursor are considered to be in effect,
-    /// and all events after the cursor are considered to not have happened
-    /// yet.
-    ///
-    /// Args:
-    /// * `event_id`: The index of the event to set the cursor to point
-    /// immediately after. If out of bounds, the cursor is set to the first or
-    /// last valid position, as appropriate.
-    pub fn set_cursor(&mut self, event_id: isize) {
+    /// If the event ID is too low or too high, it will be clamped to the valid
+    /// range for event IDs.
+    pub fn make_cursor(&self, event_id: isize) -> EventCursor {
         let event_id = if event_id < 0 { 0 } else { event_id };
         let num_events: isize = self.events.len().try_into().unwrap();
         let event_id = if event_id > num_events {
@@ -656,7 +668,7 @@ impl EventReplayer {
         } else {
             event_id
         };
-        self.cursor_event_id = event_id;
+        EventCursor { event_id }
     }
 
     /// Advance the event cursor by the specified number of events.
@@ -665,16 +677,16 @@ impl EventReplayer {
     /// * `num_events`: The number of events to advance by. Can be positive,
     /// zero, or negative. If out of bounds, the cursor is set to the first or
     /// last valid position, as appropriate.
-    pub fn advance_cursor(&mut self, num_events: isize) {
-        self.set_cursor(self.cursor_event_id + num_events)
+    pub fn advance_cursor(&self, cursor: EventCursor, num_events: isize) -> EventCursor {
+        self.make_cursor(cursor.event_id + num_events)
     }
 
     /// Get the OID of `HEAD` at the cursor's point in time.
     ///
     /// Returns: The OID pointed to by `HEAD` at that time, or `None` if `HEAD`
     /// was never observed.
-    pub fn get_cursor_head_oid(&self) -> Option<git2::Oid> {
-        let cursor_event_id: usize = self.cursor_event_id.try_into().unwrap();
+    pub fn get_cursor_head_oid(&self, cursor: EventCursor) -> Option<git2::Oid> {
+        let cursor_event_id: usize = cursor.event_id.try_into().unwrap();
         self.events[0..cursor_event_id]
             .iter()
             .rev()
@@ -709,8 +721,12 @@ impl EventReplayer {
             })
     }
 
-    fn get_cursor_branch_oid(&self, branch_name: &str) -> anyhow::Result<Option<git2::Oid>> {
-        let cursor_event_id: usize = self.cursor_event_id.try_into().unwrap();
+    fn get_cursor_branch_oid(
+        &self,
+        cursor: EventCursor,
+        branch_name: &str,
+    ) -> anyhow::Result<Option<git2::Oid>> {
+        let cursor_event_id: usize = cursor.event_id.try_into().unwrap();
         let target_ref_name = format!("refs/heads/{}", branch_name);
         let oid = self.events[0..cursor_event_id]
             .iter()
@@ -744,9 +760,13 @@ impl EventReplayer {
     ///
     /// Returns: A mapping from an OID to the names of branches pointing to that
     /// OID.
-    pub fn get_cursor_main_branch_oid(&self, repo: &git2::Repository) -> anyhow::Result<git2::Oid> {
+    pub fn get_cursor_main_branch_oid(
+        &self,
+        cursor: EventCursor,
+        repo: &git2::Repository,
+    ) -> anyhow::Result<git2::Oid> {
         let main_branch_name = get_main_branch_name(&repo)?;
-        let main_branch_oid = self.get_cursor_branch_oid(&main_branch_name)?;
+        let main_branch_oid = self.get_cursor_branch_oid(cursor, &main_branch_name)?;
         match main_branch_oid {
             Some(main_branch_oid) => Ok(main_branch_oid),
             None => {
@@ -769,10 +789,11 @@ impl EventReplayer {
     /// OID.
     pub fn get_cursor_branch_oid_to_names(
         &self,
+        cursor: EventCursor,
         repo: &git2::Repository,
     ) -> anyhow::Result<HashMap<git2::Oid, HashSet<String>>> {
         let mut ref_name_to_oid: HashMap<&String, git2::Oid> = HashMap::new();
-        let cursor_event_id: usize = self.cursor_event_id.try_into().unwrap();
+        let cursor_event_id: usize = cursor.event_id.try_into().unwrap();
         for event in self.events[..cursor_event_id].iter() {
             match event {
                 Event::RefUpdateEvent {
@@ -809,7 +830,7 @@ impl EventReplayer {
         }
 
         let main_branch_name = get_main_branch_name(&repo)?;
-        let main_branch_oid = self.get_cursor_main_branch_oid(repo)?;
+        let main_branch_oid = self.get_cursor_main_branch_oid(cursor, repo)?;
         result
             .entry(main_branch_oid)
             .or_insert_with(HashSet::new)
@@ -821,12 +842,12 @@ impl EventReplayer {
     ///
     /// Returns: A tuple of event ID and the event that most recently happened.
     /// If no event was before the event cursor, returns `None` instead.
-    pub fn get_event_before_cursor(&self) -> Option<(isize, &Event)> {
-        if self.cursor_event_id == 0 {
+    pub fn get_event_before_cursor(&self, cursor: EventCursor) -> Option<(isize, &Event)> {
+        if cursor.event_id == 0 {
             None
         } else {
-            let previous_cursor_event_id: usize = (self.cursor_event_id - 1).try_into().unwrap();
-            Some((self.cursor_event_id, &self.events[previous_cursor_event_id]))
+            let previous_cursor_event_id: usize = (cursor.event_id - 1).try_into().unwrap();
+            Some((cursor.event_id, &self.events[previous_cursor_event_id]))
         }
     }
 
@@ -834,8 +855,8 @@ impl EventReplayer {
     ///
     /// Returns: An ordered list of events that have happened since the event
     /// cursor, from least recent to most recent.
-    pub fn get_events_since_cursor(&self) -> &[Event] {
-        let cursor_event_id: usize = self.cursor_event_id.try_into().unwrap();
+    pub fn get_events_since_cursor(&self, cursor: EventCursor) -> &[Event] {
+        let cursor_event_id: usize = cursor.event_id.try_into().unwrap();
         &self.events[cursor_event_id..]
     }
 }
@@ -867,8 +888,9 @@ mod tests {
             message: None,
         });
 
+        let cursor = replayer.make_default_cursor();
         assert_eq!(
-            replayer.get_event_before_cursor(),
+            replayer.get_event_before_cursor(cursor),
             Some((1, &meaningful_event))
         );
         Ok(())

src/gc.rs
@@ -96,6 +96,7 @@ pub fn gc(out: &mut impl Write) -> anyhow::Result<()> {
         &repo,
         &merge_base_db,
         &event_replayer,
+        event_replayer.make_default_cursor(),
         &HeadOid(head_oid),
         &MainBranchOid(main_branch_oid),
         &BranchOids(branch_oid_to_names.keys().copied().collect()),

src/hide.rs
@@ -81,6 +81,7 @@ fn recurse_on_commits<'repo, F: Fn(&Node) -> bool>(
         repo,
         merge_base_db,
         event_replayer,
+        event_replayer.make_default_cursor(),
         &HeadOid(head_oid),
         &MainBranchOid(main_branch_oid),
         &BranchOids(branch_oid_to_names.keys().copied().collect()),
@@ -148,6 +149,7 @@ pub fn hide(out: &mut impl Write, hashes: Vec<String>, recursive: bool) -> anyho
         .collect();
     event_log_db.add_events(events)?;
 
+    let cursor = event_replayer.make_default_cursor();
     for commit in commits {
         let hidden_commit_text = {
             render_commit_metadata(
@@ -160,7 +162,7 @@ pub fn hide(out: &mut impl Write, hashes: Vec<String>, recursive: bool) -> anyho
         };
         writeln!(out, "Hid commit: {}", hidden_commit_text)?;
         if let Some(CommitVisibility::Hidden) =
-            event_replayer.get_cursor_commit_visibility(commit.id())
+            event_replayer.get_cursor_commit_visibility(cursor, commit.id())
         {
             writeln!(
                 out,
@@ -226,6 +228,7 @@ pub fn unhide(out: &mut impl Write, hashes: Vec<String>, recursive: bool) -> any
         .collect();
     event_log_db.add_events(events)?;
 
+    let cursor = event_replayer.make_default_cursor();
     for commit in commits {
         let unhidden_commit_text = {
             render_commit_metadata(
@@ -238,7 +241,7 @@ pub fn unhide(out: &mut impl Write, hashes: Vec<String>, recursive: bool) -> any
         };
         writeln!(out, "Unhid commit: {}", unhidden_commit_text)?;
         if let Some(CommitVisibility::Visible) =
-            event_replayer.get_cursor_commit_visibility(commit.id())
+            event_replayer.get_cursor_commit_visibility(cursor, commit.id())
         {
             writeln!(out, "(It was not hidden, so this operation had no effect.)")?;
         }

src/hooks.rs
@@ -124,6 +124,7 @@ pub fn hook_post_rewrite(out: &mut impl Write, rewrite_type: &str) -> anyhow::Re
         &repo,
         &merge_base_db,
         &event_replayer,
+        event_replayer.make_default_cursor(),
         &HeadOid(head_oid),
         &MainBranchOid(main_branch_oid),
         &BranchOids(branch_oid_to_names.keys().copied().collect()),

src/navigation.rs
@@ -163,6 +163,7 @@ pub fn next(
         &repo,
         &merge_base_db,
         &event_replayer,
+        event_replayer.make_default_cursor(),
         &HeadOid(Some(head_oid)),
         &MainBranchOid(main_branch_oid),
         &BranchOids(branch_oid_to_names.keys().copied().collect()),

src/restack.rs
@@ -77,7 +77,8 @@ fn find_rewrite_target(
     event_replayer: &EventReplayer,
     oid: git2::Oid,
 ) -> Option<git2::Oid> {
-    let event = event_replayer.get_cursor_commit_latest_event(oid);
+    let cursor = event_replayer.make_default_cursor();
+    let event = event_replayer.get_cursor_commit_latest_event(cursor, oid);
     let event = match event {
         Some(event) => event,
         None => return None,
@@ -169,6 +170,7 @@ fn restack_commits(
         repo,
         merge_base_db,
         &event_replayer,
+        event_replayer.make_default_cursor(),
         &HeadOid(head_oid),
         &MainBranchOid(main_branch_oid),
         &BranchOids(branch_oid_to_names.keys().copied().collect()),
@@ -240,6 +242,7 @@ fn restack_branches(
         repo,
         merge_base_db,
         &event_replayer,
+        event_replayer.make_default_cursor(),
         &HeadOid(head_oid),
         &MainBranchOid(main_branch_oid),
         &BranchOids(branch_oid_to_names.keys().copied().collect()),

commit 2bf8aac5a1e8634247d579d64d11f4b397d3fa7b

graph: fix incorrect historical commit visibility
This probably regressed in 07d892e5, but since it only affects node visibility and not the structure of the graph, none of the existing tests caught it.

src/core/graph.rs
@@ -152,11 +152,11 @@ fn walk_from_commits<'repo>(
     repo: &'repo git2::Repository,
     merge_base_db: &MergeBaseDb,
     event_replayer: &EventReplayer,
+    event_cursor: EventCursor,
     main_branch_oid: &MainBranchOid,
     commit_oids: &CommitOids,
 ) -> anyhow::Result<CommitGraph<'repo>> {
     let mut graph: CommitGraph = Default::default();
-    let cursor = event_replayer.make_default_cursor();
 
     for commit_oid in &commit_oids.0 {
         let commit = repo.find_commit(*commit_oid);
@@ -200,7 +200,7 @@ fn walk_from_commits<'repo>(
             }
 
             let visibility =
-                event_replayer.get_cursor_commit_visibility(cursor, current_commit.id());
+                event_replayer.get_cursor_commit_visibility(event_cursor, current_commit.id());
             let is_visible = match visibility {
                 Some(CommitVisibility::Visible) | None => true,
                 Some(CommitVisibility::Hidden) => false,
@@ -212,7 +212,7 @@ fn walk_from_commits<'repo>(
             };
 
             let event = event_replayer
-                .get_cursor_commit_latest_event(cursor, current_commit.id())
+                .get_cursor_commit_latest_event(event_cursor, current_commit.id())
                 .cloned();
             graph.insert(
                 current_commit.id(),
@@ -368,6 +368,7 @@ pub fn make_graph<'repo>(
         repo,
         merge_base_db,
         event_replayer,
+        event_cursor,
         main_branch_oid,
         commit_oids,
     )?;

tests/test_undo.rs
@@ -440,3 +440,39 @@ fn test_undo_move_refs() -> anyhow::Result<()> {
         Ok(())
     })
 }
+
+#[test]
+fn test_historical_smartlog_visibility() -> anyhow::Result<()> {
+    with_git(|git| {
+        git.init_repo()?;
+        git.commit_file("test1", 1)?;
+        git.run(&["hide", "HEAD"])?;
+
+        let screenshot1 = Default::default();
+        let screenshot2 = Default::default();
+        run_select_past_event(
+            &git.get_repo()?,
+            vec![
+                CursiveTestingEvent::TakeScreenshot(Rc::clone(&screenshot1)),
+                CursiveTestingEvent::Event('p'.into()),
+                CursiveTestingEvent::TakeScreenshot(Rc::clone(&screenshot2)),
+                CursiveTestingEvent::Event('q'.into()),
+            ],
+        )?;
+
+        insta::assert_snapshot!(screen_to_string(&screenshot1), @r###"
+        :
+        % 62fc20d2 (master) create test1.txt
+        Repo after transaction 3 (event 4). Press 'h' for help, 'q' to quit.
+        1. Hide commit 62fc20d2 create test1.txt
+        "###);
+        insta::assert_snapshot!(screen_to_string(&screenshot2), @r###"
+        :
+        @ 62fc20d2 (master) create test1.txt
+        Repo after transaction 2 (event 3). Press 'h' for help, 'q' to quit.
+        1. Commit 62fc20d2 create test1.txt
+        "###);
+
+        Ok(())
+    })
+}