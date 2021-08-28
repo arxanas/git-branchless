a3fd4095ee760455eca9b73ce4d6d237e9685389
Waleed Khan <me@waleedkhan.name>
2021-08-27 17:44:03 -0700

    refactor(mergebase): move `find_path_to_merge_base` to `MergeBaseDb` interface

--- src/core/mergebase.rs
+++ src/core/mergebase.rs
@@ -14,13 +14,15 @@
 //! take a while. It can also happen when simply checking out an old commit to
 //! examine it.
 
+use std::collections::VecDeque;
+
 use eyre::Context;
 use rusqlite::OptionalExtension;
 use tracing::instrument;
 
 use crate::core::eventlog::EventReplayer;
-use crate::git::{Dag, NonZeroOid, Repo};
-use crate::tui::Effects;
+use crate::git::{Commit, Dag, NonZeroOid, Repo};
+use crate::tui::{Effects, OperationType};
 
 /// Service that can answer merge-base queries.
 pub trait MergeBaseDb: std::fmt::Debug {
@@ -32,6 +34,29 @@ pub trait MergeBaseDb: std::fmt::Debug {
         lhs_oid: NonZeroOid,
         rhs_oid: NonZeroOid,
     ) -> eyre::Result<Option<NonZeroOid>>;
+
+    /// Find a shortest path between the given commits.
+    ///
+    /// This is particularly important for multi-parent commits (i.e. merge commits).
+    /// If we don't happen to traverse the correct parent, we may end up traversing a
+    /// huge amount of commit history, with a significant performance hit.
+    ///
+    /// Args:
+    /// * `repo`: The Git repository.
+    /// * `commit_oid`: The OID of the commit to start at. We take parents of the
+    /// provided commit until we end up at the target OID.
+    /// * `target_oid`: The OID of the commit to end at.
+    ///
+    /// Returns: A path of commits from `commit_oid` through parents to `target_oid`.
+    /// The path includes `commit_oid` at the beginning and `target_oid` at the end.
+    /// If there is no such path, returns `None`.
+    fn find_path_to_merge_base<'repo>(
+        &self,
+        effects: &Effects,
+        repo: &'repo Repo,
+        commit_oid: NonZeroOid,
+        target_oid: NonZeroOid,
+    ) -> eyre::Result<Option<Vec<Commit<'repo>>>>;
 }
 
 /// On-disk cache for merge-base queries.
@@ -69,6 +94,51 @@ impl<'conn> SqliteMergeBaseDb<'conn> {
         init_tables(conn).wrap_err("Initializing tables")?;
         Ok(SqliteMergeBaseDb { conn })
     }
+}
+
+fn find_path_to_merge_base_internal<'repo>(
+    effects: &Effects,
+    repo: &'repo Repo,
+    merge_base_db: &impl MergeBaseDb,
+    commit_oid: NonZeroOid,
+    target_oid: NonZeroOid,
+    mut visited_commit_callback: impl FnMut(NonZeroOid),
+) -> eyre::Result<Option<Vec<Commit<'repo>>>> {
+    let (effects, _progress) = effects.start_operation(OperationType::FindPathToMergeBase);
+
+    let mut queue = VecDeque::new();
+    visited_commit_callback(commit_oid);
+    let first_commit = match repo.find_commit(commit_oid)? {
+        Some(commit) => commit,
+        None => eyre::bail!("Unable to find commit with OID: {:?}", commit_oid),
+    };
+    queue.push_back(vec![first_commit]);
+    let merge_base_oid =
+        merge_base_db.get_merge_base_oid(&effects, repo, commit_oid, target_oid)?;
+    while let Some(path) = queue.pop_front() {
+        let last_commit = path
+            .last()
+            .expect("find_path_to_merge_base: empty path in queue");
+        if last_commit.get_oid() == target_oid {
+            return Ok(Some(path));
+        }
+        if Some(last_commit.get_oid()) == merge_base_oid {
+            // We've hit the common ancestor of these two commits without
+            // finding a path between them. That means it's impossible to find a
+            // path between them by traversing more ancestors. Possibly the
+            // caller passed them in in the wrong order, i.e. `commit_oid` is
+            // actually a parent of `target_oid`.
+            continue;
+        }
+
+        for parent in last_commit.get_parents() {
+            visited_commit_callback(parent.get_oid());
+            let mut new_path = path.clone();
+            new_path.push(parent);
+            queue.push_back(new_path);
+        }
+    }
+    Ok(None)
 }
 
 impl MergeBaseDb for SqliteMergeBaseDb<'_> {
@@ -155,6 +225,17 @@ INSERT INTO merge_base_oids VALUES (
             }
         }
     }
+
+    #[instrument]
+    fn find_path_to_merge_base<'repo>(
+        &self,
+        effects: &Effects,
+        repo: &'repo Repo,
+        commit_oid: NonZeroOid,
+        target_oid: NonZeroOid,
+    ) -> eyre::Result<Option<Vec<Commit<'repo>>>> {
+        find_path_to_merge_base_internal(effects, repo, self, commit_oid, target_oid, |_commit| {})
+    }
 }
 
 /// Instantiate a `MergeBaseDb` based on the requested compile-time feature.
@@ -177,4 +258,54 @@ pub fn make_merge_base_db<'conn>(
     _event_replayer: &EventReplayer,
 ) -> eyre::Result<SqliteMergeBaseDb<'conn>> {
     Ok(SqliteMergeBaseDb::new(conn)?)
+}
+
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+
+    use std::collections::HashSet;
+
+    use crate::core::eventlog::EventLogDb;
+    use crate::core::formatting::Glyphs;
+    use crate::core::mergebase::make_merge_base_db;
+    use crate::testing::make_git;
+
+    #[test]
+    fn test_find_path_to_merge_base_stop_early() -> eyre::Result<()> {
+        let git = make_git()?;
+
+        git.init_repo()?;
+        let test1_oid = git.commit_file("test1", 1)?;
+        let test2_oid = git.commit_file("test2", 2)?;
+        git.detach_head()?;
+        let test3_oid = git.commit_file("test3", 3)?;
+
+        let mut effects = Effects::new_suppress_for_test(Glyphs::detect());
+        let repo = git.get_repo()?;
+        let conn = repo.get_db_conn()?;
+        let event_log_db = EventLogDb::new(&conn)?;
+        let event_replayer = EventReplayer::from_event_log_db(&effects, &repo, &event_log_db)?;
+        let merge_base_db = make_merge_base_db(&effects, &repo, &conn, &event_replayer)?;
+
+        let mut seen_oids = HashSet::new();
+        let path = find_path_to_merge_base_internal(
+            &mut effects,
+            &repo,
+            &merge_base_db,
+            test2_oid,
+            test3_oid,
+            |oid| {
+                seen_oids.insert(oid);
+            },
+        )?;
+        assert!(path.is_none());
+
+        assert!(seen_oids.contains(&test2_oid));
+        assert!(!seen_oids.contains(&test3_oid));
+        assert!(!seen_oids.contains(&test1_oid));
+
+        Ok(())
+    }
 }
