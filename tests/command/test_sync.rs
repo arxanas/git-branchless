972662df7d632c6d2d1dbc9312a4e77ce053dbce
Waleed Khan <me@waleedkhan.name>
2022-03-12 19:10:08 -0800

    feat(sync): add test for `git sync --pull`

--- tests/command/test_sync.rs
+++ tests/command/test_sync.rs
@@ -1,4 +1,6 @@
-use branchless::testing::make_git;
+use branchless::testing::{
+    make_git, make_git_with_remote_repo, GitInitOptions, GitWrapperWithRemoteRepo,
+};
 
 #[test]
 fn test_sync_basic() -> eyre::Result<()> {
@@ -95,5 +97,73 @@ fn test_sync_up_to_date() -> eyre::Result<()> {
         insta::assert_snapshot!(stdout, @"Not moving up-to-date stack at 70deb1e2 create test3.txt
 ");
     }
+    Ok(())
+}
+
+#[test]
+fn test_sync_pull() -> eyre::Result<()> {
+    let GitWrapperWithRemoteRepo {
+        temp_dir: _guard,
+        original_repo,
+        cloned_repo,
+    } = make_git_with_remote_repo()?;
+
+    original_repo.init_repo()?;
+    original_repo.commit_file("test1", 1)?;
+    original_repo.commit_file("test2", 2)?;
+    original_repo.run(&["checkout", "-b", "foo"])?;
+    original_repo.commit_file("test3", 3)?;
+
+    original_repo.clone_repo_into(&cloned_repo, &["--branch", "master"])?;
+    cloned_repo.init_repo_with_options(&GitInitOptions {
+        make_initial_commit: false,
+        ..Default::default()
+    })?;
+    cloned_repo.run(&["checkout", "foo"])?;
+
+    original_repo.commit_file("test4", 4)?;
+    original_repo.run(&["checkout", "master"])?;
+    original_repo.commit_file("test5", 5)?;
+
+    {
+        let (stdout, _stderr) = cloned_repo.run(&["smartlog"])?;
+        insta::assert_snapshot!(stdout, @r###"
+        :
+        O 96d1c37a (master, remote origin/master) create test2.txt
+        |
+        @ 70deb1e2 (> foo) create test3.txt
+        "###);
+    }
+
+    {
+        let (stdout, _stderr) = cloned_repo.run(&["sync", "-p"])?;
+        insta::assert_snapshot!(stdout, @r###"
+        branchless: running command: <git-executable> fetch --all
+        Fetching origin
+        Attempting rebase in-memory...
+        [1/1] Committed as: 8e521a10 create test3.txt
+        branchless: processing 1 update: branch foo
+        branchless: processing 1 rewritten commit
+        branchless: running command: <git-executable> checkout foo
+        Your branch and 'origin/foo' have diverged,
+        and have 2 and 2 different commits each, respectively.
+          (use "git pull" to merge the remote branch into yours)
+        In-memory rebase succeeded.
+        Synced 70deb1e2 create test3.txt
+        "###);
+    }
+
+    {
+        let (stdout, _stderr) = cloned_repo.run(&["smartlog"])?;
+        insta::assert_snapshot!(stdout, @r###"
+        :
+        O 96d1c37a (master) create test2.txt
+        |
+        O d2e18e38 (remote origin/master) create test5.txt
+        |
+        @ 8e521a10 (> foo) create test3.txt
+        "###);
+    }
+
     Ok(())
 }
