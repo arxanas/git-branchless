fba959ed22a1c2a70d9b1c80ec92528453631cf6
Waleed Khan <me@waleedkhan.name>
2022-04-05 16:26:52 -0700

    feat(record): create `git-record` binary
    This application/library would provide an interactive terminal interface to stage certain changes. It's based on the TUI used in Mercurial's `hg crecord` extension.

---
+++ git-record/src/main.rs
@@ -0,0 +1,189 @@
+mod cursive_utils;
+mod tristate;
+mod ui;
+
+use std::io;
+use std::path::PathBuf;
+
+use cursive::backends::crossterm;
+use cursive::CursiveRunnable;
+use cursive_buffered_backend::BufferedBackend;
+use cursive_utils::EventDrivenCursiveAppExt;
+use ui::Recorder;
+
+#[derive(Clone, Debug)]
+pub struct HunkChangedLine<'a> {
+    is_selected: bool,
+    line: &'a str,
+}
+
+#[derive(Clone, Debug)]
+pub enum Hunk<'a> {
+    Unchanged {
+        contents: &'a str,
+    },
+    Changed {
+        before: Vec<HunkChangedLine<'a>>,
+        after: Vec<HunkChangedLine<'a>>,
+        parent: Option<&'a str>,
+    },
+}
+
+#[derive(Clone, Debug)]
+pub struct FileHunks<'a> {
+    hunks: Vec<Hunk<'a>>,
+}
+
+impl FileHunks<'_> {
+    pub fn get_selected_contents(&self) -> (String, String) {
+        let mut acc_selected = String::new();
+        let mut acc_unselected = String::new();
+        for hunk in &self.hunks {
+            match hunk {
+                Hunk::Unchanged { contents } => {
+                    acc_selected.push_str(contents);
+                    acc_unselected.push_str(contents);
+                }
+                Hunk::Changed {
+                    before,
+                    after,
+                    parent: _,
+                } => {
+                    for HunkChangedLine { is_selected, line } in before {
+                        // Note the inverted condition here.
+                        if !*is_selected {
+                            acc_selected.push_str(line);
+                            acc_selected.push('\n');
+                        } else {
+                            acc_unselected.push_str(line);
+                            acc_unselected.push('\n');
+                        }
+                    }
+
+                    for HunkChangedLine { is_selected, line } in after {
+                        if *is_selected {
+                            acc_selected.push_str(line);
+                            acc_selected.push('\n');
+                        } else {
+                            acc_unselected.push_str(line);
+                            acc_unselected.push('\n');
+                        }
+                    }
+                }
+            }
+        }
+        (acc_selected, acc_unselected)
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct RecordState<'a> {
+    files: Vec<(PathBuf, FileHunks<'a>)>,
+}
+
+#[derive(Clone, Debug)]
+pub enum RecordError {
+    Cancelled,
+}
+
+fn main() {
+    let preamble = "this is some text\n".repeat(20);
+    let files = {
+        let mut acc = Vec::new();
+        acc.push((
+            PathBuf::from("foo/bar"),
+            FileHunks {
+                hunks: vec![
+                    Hunk::Unchanged {
+                        contents: &preamble,
+                    },
+                    Hunk::Changed {
+                        before: vec![
+                            HunkChangedLine {
+                                is_selected: true,
+                                line: "before text 1",
+                            },
+                            HunkChangedLine {
+                                is_selected: true,
+                                line: "before text 2",
+                            },
+                        ],
+                        after: vec![
+                            HunkChangedLine {
+                                is_selected: true,
+                                line: "after text 1",
+                            },
+                            HunkChangedLine {
+                                is_selected: false,
+                                line: "after text 2",
+                            },
+                        ],
+                        parent: Some("parent line 1\nparent line 2\n"),
+                    },
+                    Hunk::Unchanged {
+                        contents: "this is some trailing text\n",
+                    },
+                ],
+            },
+        ));
+        acc.push((
+            PathBuf::from("baz"),
+            FileHunks {
+                hunks: vec![
+                    Hunk::Unchanged {
+                        contents: &"Some leading text 1\nSome leading text 2\n",
+                    },
+                    Hunk::Changed {
+                        before: vec![
+                            HunkChangedLine {
+                                is_selected: true,
+                                line: "before text 1",
+                            },
+                            HunkChangedLine {
+                                is_selected: true,
+                                line: "before text 2",
+                            },
+                        ],
+                        after: vec![
+                            HunkChangedLine {
+                                is_selected: true,
+                                line: "after text 1",
+                            },
+                            HunkChangedLine {
+                                is_selected: true,
+                                line: "after text 2",
+                            },
+                        ],
+                        parent: Some("parent line 1\nparent line 2\n"),
+                    },
+                    Hunk::Unchanged {
+                        contents: "this is some trailing text\n",
+                    },
+                ],
+            },
+        ));
+        acc
+    };
+    let record_state = RecordState { files };
+
+    // TODO: let user select backend
+    // let mut siv = cursive::default();
+    let siv = CursiveRunnable::new(|| -> io::Result<_> {
+        // Use crossterm to ensure that we support Windows.
+        let crossterm_backend = crossterm::Backend::init()?;
+        Ok(Box::new(BufferedBackend::new(crossterm_backend)))
+    });
+    let siv = siv.into_runner();
+
+    let recorder = Recorder::new(record_state);
+    let result = recorder.run(siv);
+    let RecordState { files: result } = match result {
+        Ok(result) => result,
+        Err(RecordError::Cancelled) => todo!("Cancelled"),
+    };
+    for (path, file_hunks) in result {
+        println!("Path {}", path.display());
+        let (selected, _unselected) = file_hunks.get_selected_contents();
+        print!("{}", selected);
+    }
+}
---
+++ git-record/src/tristate.rs
@@ -0,0 +1,126 @@
+use cursive::{
+    direction::Direction,
+    event::{Event, EventResult, Key, MouseButton, MouseEvent},
+    impl_enabled,
+    theme::ColorStyle,
+    view::{CannotFocus, View},
+    Cursive, Printer, Vec2, With,
+};
+use std::rc::Rc;
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub enum Tristate {
+    Unchecked,
+    Partial,
+    Checked,
+}
+
+/// Three-state checkable box.
+pub struct TristateBox {
+    state: Tristate,
+    enabled: bool,
+
+    on_change: Option<Rc<dyn Fn(&mut Cursive, Tristate)>>,
+}
+
+impl TristateBox {
+    impl_enabled!(self.enabled);
+
+    /// Creates a new, unset tristate box.
+    pub fn new() -> Self {
+        Self {
+            state: Tristate::Unchecked,
+            enabled: true,
+            on_change: None,
+        }
+    }
+
+    /// Sets a callback to be used when the state changes.
+    pub fn set_on_change<F: 'static + Fn(&mut Cursive, Tristate)>(&mut self, on_change: F) {
+        self.on_change = Some(Rc::new(on_change));
+    }
+
+    /// Sets a callback to be used when the state changes.
+    ///
+    /// Chainable variant.
+    #[must_use]
+    pub fn on_change<F: 'static + Fn(&mut Cursive, Tristate)>(self, on_change: F) -> Self {
+        self.with(|s| s.set_on_change(on_change))
+    }
+
+    /// Toggles the checkbox state.
+    pub fn toggle(&mut self) -> EventResult {
+        let state = match self.state {
+            Tristate::Unchecked | Tristate::Partial => Tristate::Checked,
+            Tristate::Checked => Tristate::Unchecked,
+        };
+        self.set_state(state)
+    }
+
+    /// Sets the checkbox state.
+    pub fn set_state(&mut self, state: Tristate) -> EventResult {
+        self.state = state;
+        if let Some(ref on_change) = self.on_change {
+            let on_change = Rc::clone(on_change);
+            EventResult::with_cb(move |s| on_change(s, state))
+        } else {
+            EventResult::Consumed(None)
+        }
+    }
+
+    /// Set the checkbox state.
+    ///
+    /// Chainable variant.
+    #[must_use]
+    pub fn with_state(self, state: Tristate) -> Self {
+        self.with(|s| {
+            s.set_state(state);
+        })
+    }
+
+    fn draw_internal(&self, printer: &Printer) {
+        printer.print(
+            (0, 0),
+            match self.state {
+                Tristate::Unchecked => "[ ]",
+                Tristate::Partial => "[~]",
+                Tristate::Checked => "[X]",
+            },
+        );
+    }
+}
+
+impl View for TristateBox {
+    fn required_size(&mut self, _: Vec2) -> Vec2 {
+        Vec2::new(3, 1)
+    }
+
+    fn take_focus(&mut self, _: Direction) -> Result<EventResult, CannotFocus> {
+        self.enabled.then(EventResult::consumed).ok_or(CannotFocus)
+    }
+
+    fn draw(&self, printer: &Printer) {
+        if self.enabled && printer.enabled {
+            printer.with_selection(printer.focused, |printer| self.draw_internal(printer));
+        } else {
+            printer.with_color(ColorStyle::secondary(), |printer| {
+                self.draw_internal(printer)
+            });
+        }
+    }
+
+    fn on_event(&mut self, event: Event) -> EventResult {
+        if !self.enabled {
+            return EventResult::Ignored;
+        }
+        match event {
+            Event::Key(Key::Enter) | Event::Char(' ') => self.toggle(),
+            Event::Mouse {
+                event: MouseEvent::Release(MouseButton::Left),
+                position,
+                offset,
+            } if position.fits_in_rect(offset, (3, 1)) => self.toggle(),
+            _ => EventResult::Ignored,
+        }
+    }
+}
