arxanas/git-branchless

commit 24beb1b8e3dd0e19bfffa2e56376e4c15364c772

fix(gc): mark commits reachable in `Dag::sync_from_oids`

git-branchless-lib/src/core/dag.rs
@@ -15,6 +15,7 @@ use tracing::{instrument, trace, warn};
 
 use crate::core::effects::{Effects, OperationType};
 use crate::core::eventlog::{CommitActivityStatus, EventCursor, EventReplayer};
+use crate::core::gc::mark_commit_reachable;
 use crate::git::{Commit, MaybeZeroOid, NonZeroOid, Repo};
 
 use super::repo_ext::RepoReferencesSnapshot;
@@ -25,13 +26,23 @@ impl From<NonZeroOid> for eden_dag::VertexName {
     }
 }
 
-impl TryFrom<eden_dag::VertexName> for NonZeroOid {
+impl TryFrom<eden_dag::VertexName> for MaybeZeroOid {
     type Error = eyre::Error;
 
     fn try_from(value: eden_dag::VertexName) -> Result<Self, Self::Error> {
         let oid = git2::Oid::from_bytes(value.as_ref())?;
         let oid = MaybeZeroOid::from(oid);
-        NonZeroOid::try_from(oid)
+        Ok(oid)
+    }
+}
+
+impl TryFrom<eden_dag::VertexName> for NonZeroOid {
+    type Error = eyre::Error;
+
+    fn try_from(value: eden_dag::VertexName) -> Result<Self, Self::Error> {
+        let oid = MaybeZeroOid::try_from(value)?;
+        let oid = NonZeroOid::try_from(oid)?;
+        Ok(oid)
     }
 }
 
@@ -181,7 +192,8 @@ impl Dag {
     }
 
     /// This function's code adapted from `GitDag`, licensed under GPL-2.
-    fn sync(&mut self, effects: &Effects, repo: &Repo) -> eden_dag::Result<()> {
+    #[instrument]
+    fn sync(&mut self, effects: &Effects, repo: &Repo) -> eyre::Result<()> {
         let master_heads = self.main_branch_commit.clone();
         let non_master_heads = self
             .observed_commits
@@ -191,15 +203,45 @@ impl Dag {
     }
 
     /// Update the DAG with the given heads.
+    #[instrument]
     pub fn sync_from_oids(
         &mut self,
         effects: &Effects,
         repo: &Repo,
         master_heads: CommitSet,
         non_master_heads: CommitSet,
-    ) -> eden_dag::Result<()> {
+    ) -> eyre::Result<()> {
         let (effects, _progress) = effects.start_operation(OperationType::UpdateCommitGraph);
         let _effects = effects;
+
+        for head in master_heads.iter().context("Iterating master heads")? {
+            let head = head.context("Accessing master head")?;
+            let head = MaybeZeroOid::try_from(head)?;
+            match head {
+                MaybeZeroOid::Zero => {
+                    // Do nothing.
+                }
+                MaybeZeroOid::NonZero(oid) => {
+                    mark_commit_reachable(repo, oid).context("Marking master head as reachable")?;
+                }
+            }
+        }
+        for head in non_master_heads
+            .iter()
+            .context("Iterating non-master heads")?
+        {
+            let head = head.context("Accessing non-master head")?;
+            let head = MaybeZeroOid::try_from(head)?;
+            match head {
+                MaybeZeroOid::Zero => {
+                    // Do nothing.
+                }
+                MaybeZeroOid::NonZero(oid) => {
+                    mark_commit_reachable(repo, oid)
+                        .context("Marking non-master head as reachable")?;
+                }
+            }
+        }
 
         let parent_func = |v: CommitVertex| -> eden_dag::Result<Vec<CommitVertex>> {
             use eden_dag::errors::BackendError;

git-branchless/tests/command/test_move.rs
@@ -2096,7 +2096,7 @@ fn test_move_orig_head_no_symbolic_reference() -> eyre::Result<()> {
 }
 
 #[test]
-fn test_move_standalone_no_create_gc_refs() -> eyre::Result<()> {
+fn test_move_standalone_create_gc_refs() -> eyre::Result<()> {
     let git = make_git()?;
 
     git.init_repo_with_options(&GitInitOptions {
@@ -2107,21 +2107,22 @@ fn test_move_standalone_no_create_gc_refs() -> eyre::Result<()> {
     git.run(&["checkout", "HEAD^"])?;
     git.commit_file("test2", 2)?;
 
-    let show_refs_output = {
+    {
         let (stdout, _stderr) = git.run(&["show-ref"])?;
         insta::assert_snapshot!(stdout, @"62fc20d2a290daea0d52bdc2ed2ad4be6491010e refs/heads/master
 ");
-        stdout
-    };
+    }
 
     git.run(&["branchless", "move", "-d", &test1_oid.to_string()])?;
 
     {
         let (stdout, _stderr) = git.run(&["show-ref"])?;
         insta::assert_snapshot!(stdout, @r###"
+        62fc20d2a290daea0d52bdc2ed2ad4be6491010e refs/branchless/62fc20d2a290daea0d52bdc2ed2ad4be6491010e
+        96d1c37a3d4363611c49f7e52186e189a04c531f refs/branchless/96d1c37a3d4363611c49f7e52186e189a04c531f
+        fe65c1fe15584744e649b2c79d4cf9b0d878f92e refs/branchless/fe65c1fe15584744e649b2c79d4cf9b0d878f92e
         62fc20d2a290daea0d52bdc2ed2ad4be6491010e refs/heads/master
         "###);
-        assert!(stdout == show_refs_output);
     }
 
     Ok(())

git-branchless-lib/src/core/gc.rs
@@ -43,7 +43,12 @@ pub fn find_dangling_references<'repo>(
             CommitActivityStatus::Active => {
                 // Do nothing.
             }
-            CommitActivityStatus::Inactive | CommitActivityStatus::Obsolete => {
+            CommitActivityStatus::Inactive => {
+                // This commit hasn't been observed, but it's possible that the user expected it
+                // to remain. Do nothing. See https://github.com/arxanas/git-branchless/issues/412.
+            }
+            CommitActivityStatus::Obsolete => {
+                // This commit was explicitly hidden by some operation.
                 result.push(reference)
             }
         }
@@ -57,30 +62,31 @@ pub fn find_dangling_references<'repo>(
 /// collection mechanism until first garbage-collected by branchless itself
 /// (using the `gc` function).
 ///
+/// If the commit does not exist (such as if it was already garbage-collected), then this is a no-op.
+///
 /// Args:
 /// * `repo`: The Git repository.
 /// * `commit_oid`: The commit OID to mark as reachable.
 #[instrument]
 pub fn mark_commit_reachable(repo: &Repo, commit_oid: NonZeroOid) -> eyre::Result<()> {
-    if !repo.get_config_path().exists() {
-        // Don't create garbage collection references for users who don't appear
-        // to have run `git branchless init` in this repository, since they may
-        // not want their tooling (such as `tig`) polluted by extra references.
-        // See https://github.com/arxanas/git-branchless/issues/183
-        return Ok(());
-    }
-
     let ref_name = format!("refs/branchless/{}", commit_oid);
     eyre::ensure!(
         Reference::is_valid_name(&ref_name),
         format!("Invalid ref name to mark commit as reachable: {}", ref_name)
     );
-    repo.create_reference(
-        OsStr::new(&ref_name),
-        commit_oid,
-        true,
-        "branchless: marking commit as reachable",
-    )
-    .wrap_err("Creating reference")?;
+
+    // NB: checking for the commit first with `find_commit` is racy, as the `create_reference` call
+    // could still fail if the commit is deleted by then, but it's too hard to propagate whether the
+    // commit was not found from `create_reference`.
+    if repo.find_commit(commit_oid)?.is_some() {
+        repo.create_reference(
+            OsStr::new(&ref_name),
+            commit_oid,
+            true,
+            "branchless: marking commit as reachable",
+        )
+        .wrap_err("Creating reference")?;
+    }
+
     Ok(())
 }

git-branchless/src/revset/eval.rs
@@ -35,6 +35,9 @@ pub enum EvalError {
         #[from]
         from: eden_dag::Error,
     },
+
+    #[error(transparent)]
+    OtherError(eyre::Error),
 }
 
 pub type EvalResult = Result<CommitSet, EvalError>;
@@ -132,12 +135,14 @@ fn eval_name(ctx: &mut Context, name: &str) -> EvalResult {
         }
     };
 
-    ctx.dag.sync_from_oids(
-        ctx.effects,
-        ctx.repo,
-        CommitSet::empty(),
-        commit_set.clone(),
-    )?;
+    ctx.dag
+        .sync_from_oids(
+            ctx.effects,
+            ctx.repo,
+            CommitSet::empty(),
+            commit_set.clone(),
+        )
+        .map_err(EvalError::OtherError)?;
     Ok(commit_set)
 }
 

git-branchless/src/revset/resolve.rs
@@ -67,7 +67,7 @@ pub fn resolve_commits(
         if let Ok(Some(commit)) = repo.revparse_single_commit(&revset) {
             let commit_set = CommitSet::from(commit.get_oid());
             dag.sync_from_oids(effects, repo, CommitSet::empty(), commit_set.clone())
-                .map_err(|err| ResolveError::DagError { source: err })?;
+                .map_err(|err| ResolveError::OtherError { source: err })?;
             commit_sets.push(commit_set);
             continue;
         }

git-branchless/tests/core/test_gc.rs
@@ -2,7 +2,7 @@ use itertools::Itertools;
 use lib::core::eventlog::testing::redact_event_timestamp;
 use lib::core::eventlog::EventLogDb;
 use lib::git::GitVersion;
-use lib::testing::make_git;
+use lib::testing::{make_git, GitInitOptions};
 
 #[test]
 fn test_gc() -> eyre::Result<()> {
@@ -206,6 +206,68 @@ fn test_gc_reference_transaction() -> eyre::Result<()> {
         },
     ]
     "###);
+
+    Ok(())
+}
+
+#[test]
+fn test_gc_no_init() -> eyre::Result<()> {
+    let git = make_git()?;
+
+    if !git.supports_reference_transactions()? {
+        return Ok(());
+    }
+    git.init_repo_with_options(&GitInitOptions {
+        run_branchless_init: false,
+        ..Default::default()
+    })?;
+
+    git.detach_head()?;
+    let test1_oid = git.commit_file("test1", 1)?;
+
+    {
+        let (stdout, _stderr) = git.run(&["branchless", "smartlog"])?;
+        insta::assert_snapshot!(stdout, @r###"
+        O f777ecc (master) create initial.txt
+        |
+        @ 62fc20d create test1.txt
+        "###);
+    }
+
+    git.run(&["checkout", "HEAD~"])?;
+    {
+        let (stdout, stderr) = git.run(&["gc", "--prune=now"])?;
+        insta::assert_snapshot!(stderr, @"");
+        insta::assert_snapshot!(stdout, @"");
+    }
+
+    git.run(&["checkout", &test1_oid.to_string()])?;
+    {
+        let (stdout, _stderr) = git.run(&["branchless", "smartlog"])?;
+        insta::assert_snapshot!(stdout, @r###"
+        O f777ecc (master) create initial.txt
+        |
+        @ 62fc20d create test1.txt
+        "###);
+    }
+
+    git.run(&["checkout", "HEAD~"])?;
+    git.run(&["branchless", "gc"])?;
+    {
+        let (stdout, stderr) = git.run(&["gc", "--prune=now"])?;
+        insta::assert_snapshot!(stderr, @"");
+        insta::assert_snapshot!(stdout, @"");
+    }
+
+    git.run(&["checkout", &test1_oid.to_string()])?;
+    {
+        let (stdout, _stderr) = git.run(&["branchless", "smartlog"])?;
+        insta::assert_snapshot!(stdout, @r###"
+        O f777ecc (master) create initial.txt
+        |
+        @ 62fc20d create test1.txt
+        "###);
+    }
 
     Ok(())
 }

commit e746c6bdf5f11ce9c17afd776d454460c6f62356

fix(sync): fix performance regression from #418
Unfortunately, adding garbage collection references for every invisible head is a significant performant regression. To fix this, we instead only mark commits as reachable if they're *active* (would appear in the smartlog). This does mean that commits which were available in the DAG but not used by the smartlog might get garbage collected, but it seems like an acceptable trade-off.

git-branchless-lib/src/core/dag.rs
@@ -15,7 +15,6 @@ use tracing::{instrument, trace, warn};
 
 use crate::core::effects::{Effects, OperationType};
 use crate::core::eventlog::{CommitActivityStatus, EventCursor, EventReplayer};
-use crate::core::gc::mark_commit_reachable;
 use crate::git::{Commit, MaybeZeroOid, NonZeroOid, Repo};
 
 use super::repo_ext::RepoReferencesSnapshot;
@@ -213,35 +212,6 @@ impl Dag {
     ) -> eyre::Result<()> {
         let (effects, _progress) = effects.start_operation(OperationType::UpdateCommitGraph);
         let _effects = effects;
-
-        for head in master_heads.iter().context("Iterating master heads")? {
-            let head = head.context("Accessing master head")?;
-            let head = MaybeZeroOid::try_from(head)?;
-            match head {
-                MaybeZeroOid::Zero => {
-                    // Do nothing.
-                }
-                MaybeZeroOid::NonZero(oid) => {
-                    mark_commit_reachable(repo, oid).context("Marking master head as reachable")?;
-                }
-            }
-        }
-        for head in non_master_heads
-            .iter()
-            .context("Iterating non-master heads")?
-        {
-            let head = head.context("Accessing non-master head")?;
-            let head = MaybeZeroOid::try_from(head)?;
-            match head {
-                MaybeZeroOid::Zero => {
-                    // Do nothing.
-                }
-                MaybeZeroOid::NonZero(oid) => {
-                    mark_commit_reachable(repo, oid)
-                        .context("Marking non-master head as reachable")?;
-                }
-            }
-        }
 
         let parent_func = |v: CommitVertex| -> eden_dag::Result<Vec<CommitVertex>> {
             use eden_dag::errors::BackendError;

git-branchless/src/commands/smartlog.rs
@@ -29,6 +29,7 @@ mod graph {
     use std::ops::Deref;
 
     use eden_dag::DagAlgorithm;
+    use lib::core::gc::mark_commit_reachable;
     use tracing::instrument;
 
     use lib::core::dag::{commit_set_to_vec_unsorted, CommitSet, Dag};
@@ -241,6 +242,9 @@ mod graph {
             };
 
             let active_heads = dag.query_active_heads(&public_commits, &observed_commits)?;
+            for oid in commit_set_to_vec_unsorted(&active_heads)? {
+                mark_commit_reachable(repo, oid)?;
+            }
 
             walk_from_active_heads(&effects, repo, dag, &public_commits, &active_heads)?
         };

git-branchless/tests/command/test_move.rs
@@ -2120,7 +2120,6 @@ fn test_move_standalone_create_gc_refs() -> eyre::Result<()> {
         insta::assert_snapshot!(stdout, @r###"
         62fc20d2a290daea0d52bdc2ed2ad4be6491010e refs/branchless/62fc20d2a290daea0d52bdc2ed2ad4be6491010e
         96d1c37a3d4363611c49f7e52186e189a04c531f refs/branchless/96d1c37a3d4363611c49f7e52186e189a04c531f
-        fe65c1fe15584744e649b2c79d4cf9b0d878f92e refs/branchless/fe65c1fe15584744e649b2c79d4cf9b0d878f92e
         62fc20d2a290daea0d52bdc2ed2ad4be6491010e refs/heads/master
         "###);
     }