afe4c84c17f01bc45451bb5cd6949cd547b7eaa1
Waleed Khan <me@waleedkhan.name>
2022-07-05 20:43:25 -0700

    build: warn no `clippy::clone_on_ref_ptr`

--- git-branchless/src/lib.rs
+++ git-branchless/src/lib.rs
@@ -17,7 +17,7 @@
 //! Then the branchless workflow may be for you instead.
 
 #![warn(missing_docs)]
-#![warn(clippy::all, clippy::as_conversions)]
+#![warn(clippy::all, clippy::as_conversions, clippy::clone_on_ref_ptr)]
 #![allow(clippy::too_many_arguments, clippy::blocks_in_if_conditions)]
 
 pub mod commands;
