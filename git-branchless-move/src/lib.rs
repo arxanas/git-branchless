d62c2cfbb5150228106f4b382f05e14e1158c96b
Waleed Khan <me@waleedkhan.name>
2022-11-13 19:50:00 -0600

    build: extract `git-branchless-move` crate

--- git-branchless-move/src/lib.rs
+++ git-branchless-move/src/lib.rs
@@ -3,6 +3,10 @@
 //! Under the hood, this makes use of Git's advanced rebase functionality, which
 //! is also used to preserve merge commits using the `--rebase-merges` option.
 
+#![warn(missing_docs)]
+#![warn(clippy::all, clippy::as_conversions, clippy::clone_on_ref_ptr)]
+#![allow(clippy::too_many_arguments, clippy::blocks_in_if_conditions)]
+
 use std::collections::HashMap;
 use std::convert::TryFrom;
 use std::fmt::Write;
