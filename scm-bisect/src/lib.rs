627c8feb8c9092c46054f450f35a378c7628e9ea
Waleed Khan <me@waleedkhan.name>
2023-02-09 20:29:45 -0800

    feat(test): create `search` module

---
+++ scm-bisect/src/lib.rs
@@ -0,0 +1,14 @@
+//! Reusable algorithms for identifying the first bad commit in a directed
+//! acyclic graph (similar to `git-bisect`). The intention is to provide support
+//! for various source control systems.
+
+#![warn(missing_docs)]
+#![warn(
+    clippy::all,
+    clippy::as_conversions,
+    clippy::clone_on_ref_ptr,
+    clippy::dbg_macro
+)]
+#![allow(clippy::too_many_arguments, clippy::blocks_in_if_conditions)]
+
+pub mod search;
