2159db6109b1611409811a82c12a43b39cc10b02
Waleed Khan <me@waleedkhan.name>
2023-02-12 11:35:03 -0800

    refactor(tests): extract PTY functions into `testing` module

--- git-branchless-lib/src/testing.rs
+++ git-branchless-lib/src/testing.rs
@@ -758,3 +758,156 @@ pub fn trim_lines(output: String) -> String {
         .flat_map(|line| vec![line.trim_end(), "\n"].into_iter())
         .collect()
 }
+
+/// Utilities for testing in a virtual terminal (PTY).
+pub mod pty {
+    use std::{
+        sync::{mpsc::channel, Arc, Mutex},
+        thread,
+        time::Duration,
+    };
+
+    use eyre::eyre;
+    use portable_pty::{native_pty_system, CommandBuilder, PtySize};
+
+    use super::Git;
+
+    /// An action to take as part of the PTY test script.
+    pub enum PtyAction<'a> {
+        /// Input the provided string as keystrokes to the terminal.
+        Write(&'a str),
+
+        /// Wait until the terminal display shows the provided string anywhere
+        /// on the screen.
+        WaitUntilContains(&'a str),
+    }
+
+    /// Run the provided script in the context of a virtual terminal.
+    pub fn run_in_pty(
+        git: &Git,
+        branchless_subcommand: &str,
+        args: &[&str],
+        inputs: &[PtyAction],
+    ) -> eyre::Result<()> {
+        // Use the native pty implementation for the system
+        let pty_system = native_pty_system();
+        let pty_size = PtySize::default();
+        let mut pty = pty_system
+            .openpty(pty_size)
+            .map_err(|e| eyre!("Could not open pty: {}", e))?;
+
+        // Spawn a git instance in the pty.
+        let mut cmd = CommandBuilder::new(&git.path_to_git);
+        cmd.env_clear();
+        for (k, v) in git.get_base_env(0) {
+            cmd.env(k, v);
+        }
+        cmd.env("TERM", "xterm");
+        cmd.arg("branchless");
+        cmd.arg(branchless_subcommand);
+        cmd.args(args);
+        cmd.cwd(&git.repo_path);
+
+        let mut child = pty
+            .slave
+            .spawn_command(cmd)
+            .map_err(|e| eyre!("Could not spawn child: {}", e))?;
+
+        let reader = pty
+            .master
+            .try_clone_reader()
+            .map_err(|e| eyre!("Could not clone reader: {}", e))?;
+        let reader = Arc::new(Mutex::new(reader));
+
+        let parser = vt100::Parser::new(pty_size.rows, pty_size.cols, 0);
+        let parser = Arc::new(Mutex::new(parser));
+
+        for action in inputs {
+            match action {
+                PtyAction::WaitUntilContains(value) => {
+                    let (finished_tx, finished_rx) = channel();
+
+                    let wait_thread = {
+                        let parser = Arc::clone(&parser);
+                        let reader = Arc::clone(&reader);
+                        let value = value.to_string();
+                        thread::spawn(move || -> anyhow::Result<()> {
+                            loop {
+                                // Drop the `parser` lock after this, since we may block
+                                // on `reader.read` below, and the caller may want to
+                                // check the screen contents of `parser`.
+                                {
+                                    let parser = parser.lock().unwrap();
+                                    if parser.screen().contents().contains(&value) {
+                                        break;
+                                    }
+                                }
+
+                                let mut reader = reader.lock().unwrap();
+                                const BUF_SIZE: usize = 4096;
+                                let mut buffer = [0; BUF_SIZE];
+                                let n = reader.read(&mut buffer)?;
+                                assert!(
+                                    n < BUF_SIZE,
+                                    "filled up PTY buffer by reading {} bytes",
+                                    n
+                                );
+
+                                {
+                                    let mut parser = parser.lock().unwrap();
+                                    parser.process(&buffer[..n]);
+                                }
+                            }
+
+                            finished_tx.send(()).unwrap();
+                            Ok(())
+                        })
+                    };
+
+                    if finished_rx.recv_timeout(Duration::from_secs(5)).is_err() {
+                        panic!(
+                            "\
+Timed out waiting for virtual terminal to show string: {:?}
+Screen contents:
+-----
+{}
+-----
+",
+                            value,
+                            parser.lock().unwrap().screen().contents(),
+                        );
+                    }
+
+                    wait_thread.join().unwrap().unwrap();
+                }
+
+                PtyAction::Write(value) => {
+                    write!(pty.master, "{value}")?;
+                    pty.master.flush()?;
+                }
+            }
+        }
+
+        let read_remainder_of_pty_output_thread = thread::spawn({
+            let reader = Arc::clone(&reader);
+            move || {
+                let mut reader = reader.lock().unwrap();
+                let mut buffer = Vec::new();
+                reader.read_to_end(&mut buffer).expect("finish reading pty");
+                String::from_utf8(buffer).unwrap()
+            }
+        });
+        child.wait()?;
+
+        let _ = read_remainder_of_pty_output_thread;
+        // Useful for debugging, but seems to deadlock on some tests:
+        // let remainder_of_pty_output = read_remainder_of_pty_output_thread.join().unwrap();
+        // assert!(
+        //     !remainder_of_pty_output.contains("panic"),
+        //     "Panic in PTY thread:\n{}",
+        //     console::strip_ansi_codes(&remainder_of_pty_output)
+        // );
+
+        Ok(())
+    }
+}
