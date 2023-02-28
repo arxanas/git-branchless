f15f479851bf850de2dc57bd018c8ce586f39192
Waleed Khan <me@waleedkhan.name>
2023-02-27 17:53:20 -0800

    feat(record): implement selection and scrolling
    This took a surprising amount of work. The main problem is determining where on the virtual canvas a given component has been rendered. When we desire to scroll to that component, we may have to move the viewport so that the component becomes visible.
    
    As I mentioned in a previous commit, it's too difficult and error-prone for each component to calculate where it *would* render. Instead, we just render everything and keep track of where each component *did* render. The main downsides:
    
    - We have to plumb "component IDs" all the way down the component tree so that we can use the ID to refer to the drawn location later.
    - In principle, we could skip drawing most of the components, since they won't be in the viewport. However, under the current scheme, we won't even be able to calculate where the components *would* be without essentially rendering them first.
    
    Once we implement this, we can straightforwardly calculate where a given component has been rendered and then ensure that we scroll such that the component is in the viewport.

--- scm-record/src/util.rs
+++ scm-record/src/util.rs
@@ -1,9 +1,39 @@
 pub(crate) trait UsizeExt {
     fn unwrap_isize(self) -> isize;
+    fn clamp_into_u16(self) -> u16;
 }
 
 impl UsizeExt for usize {
     fn unwrap_isize(self) -> isize {
         isize::try_from(self).unwrap()
+    }
+
+    fn clamp_into_u16(self) -> u16 {
+        if self > u16::MAX.try_into().unwrap() {
+            u16::MAX
+        } else {
+            self.try_into().unwrap()
+        }
+    }
+}
+
+pub(crate) trait IsizeExt {
+    fn unwrap_usize(self) -> usize;
+    fn clamp_into_u16(self) -> u16;
+}
+
+impl IsizeExt for isize {
+    fn unwrap_usize(self) -> usize {
+        usize::try_from(self).unwrap()
+    }
+
+    fn clamp_into_u16(self) -> u16 {
+        if self < 0 {
+            0
+        } else if self > u16::MAX.try_into().unwrap() {
+            u16::MAX
+        } else {
+            self.try_into().unwrap()
+        }
     }
 }
