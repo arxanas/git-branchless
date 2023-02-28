01f53f6d9b35c95e8c675077fc9041a14aa9019b
Waleed Khan <me@waleedkhan.name>
2023-02-27 17:53:19 -0800

    feat(record): create `scm-record` library

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+//! Reusable change selector UI for source control systems.
+
+#![warn(missing_docs)]
+#![warn(
+    clippy::all,
+    clippy::as_conversions,
+    clippy::clone_on_ref_ptr,
+    clippy::dbg_macro
+)]
+#![allow(clippy::too_many_arguments, clippy::blocks_in_if_conditions)]
